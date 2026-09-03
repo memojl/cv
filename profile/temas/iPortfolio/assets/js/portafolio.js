cdd09d2f55a78a70684208256253eb3622423709
multiportal <multiportal@outlook.com>
2026-09-03 13:11:48 -0600

    Update

--- profile/temas/iPortfolio/assets/js/portafolio.js
+++ profile/temas/iPortfolio/assets/js/portafolio.js
@@ -1,16 +1,17 @@
 //FUNCIONES
-async function portafolio(){
-    const url = 'https://portafolio1.webcindario.com/api/v1/?tabla=portafolio';
-    const res = await fetch(url);
-    const data = await res.json();
-    data.reverse();//console.log(data);
-    const unicos = [];
-    let porta = document.querySelector('.portfolio-container');
-    let cat = document.querySelector('#portfolio-flters');
-    let div = '';
-    data.forEach(item => {
-      const {ID, nombre, cate, cover, imagen1, descripcion, url_page, visible} = item;
-      div += `
+async function portafolio() {
+  let porta = document.querySelector('.portfolio-container');
+  let cat = document.querySelector('#portfolio-flters');
+  let div = '';
+  const url = 'https://portafolio1.webcindario.com/api/v1/?tabla=portafolio';
+  const res = await fetch(url);
+  const data = await res.json();
+  if(!data){porta.innerHTML = ``; return;}
+  data.reverse();//console.log(data);
+  const unicos = [];
+  data.forEach(item => {
+    const { ID, nombre, cate, cover, imagen1, descripcion, url_page, visible } = item;
+    div += `
       <div class="col-lg-4 col-md-6 portfolio-item filter-${cate}">
         <div class="portfolio-wrap">
           <img src="https://portafolio1.webcindario.com/modulos/portafolio/fotos/${cover}" class="img-fluid" alt="">
@@ -20,25 +21,26 @@ async function portafolio(){
           </div>
         </div>
       </div>`;
-      if (!unicos.includes(cate)) {
-        unicos.push(cate);
-      }
-    });//console.log(unicos.reverse());
-    porta.innerHTML = div;
-    unicos.reverse();
-    let li = '<li onclick="btn(0)" data-filter="*" class="filter-active">All</li>';
-    var n=0;
-    for(var i=0;i<unicos.length;i++){n++;
-      li += `<li onclick="btn(${n})" data-filter=".filter-${unicos[i]}">${unicos[i]}</li>`
+    if (!unicos.includes(cate)) {
+      unicos.push(cate);
     }
-    cat.innerHTML = li;
+  });//console.log(unicos.reverse());
+  porta.innerHTML = div;
+  unicos.reverse();
+  let li = '<li onclick="btn(0)" data-filter="*" class="filter-active">All</li>';
+  var n = 0;
+  for (var i = 0; i < unicos.length; i++) {
+    n++;
+    li += `<li onclick="btn(${n})" data-filter=".filter-${unicos[i]}">${unicos[i]}</li>`
+  }
+  cat.innerHTML = li;
 }
 
 function calcularEdad(fechaNacimiento) {
   const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-  const fec =  fechaNacimiento.split('-');
-  const m = parseInt(fec[1].replace('0',''));
-  const month = months[m-1];
+  const fec = fechaNacimiento.split('-');
+  const m = parseInt(fec[1].replace('0', ''));
+  const month = months[m - 1];
   const fechaNac = `${fec[2]}/${month}/${fec[0]}`;
   let hoy = new Date();
   let nacimiento = new Date(fechaNacimiento);
@@ -47,23 +49,23 @@ function calcularEdad(fechaNacimiento) {
   let dia = hoy.getDate() - nacimiento.getDate();
   // Ajustar la edad si aún no ha pasado el cumpleaños este año
   if (mes < 0 || (mes === 0 && dia < 0)) {
-      edad--;
+    edad--;
   }
-  
+
   console.log(fechaNac);
   const miFechaNac = document.querySelector('#miFechaNac');
-  if(miFechaNac){
+  if (miFechaNac) {
     miFechaNac.innerHTML = fechaNac;
   }
   console.log(edad);
   const miEdad = document.querySelector('#miEdad');
-  if(miEdad){
-    miEdad.innerHTML = edad+' años';
+  if (miEdad) {
+    miEdad.innerHTML = edad + ' años';
   }
 }
 
-export function inicio(){
-    console.log('Función corriendo');
-    portafolio();
-    calcularEdad('1979-04-08');
+export function inicio() {
+  console.log('Función corriendo');
+  portafolio();
+  calcularEdad('1979-04-08');
 }
