d8c0e2868ad256056a66084ec9d45d05af9c8895
multiportal <multiportal@outlook.com>
2026-09-03 13:15:24 -0600

    Update Portafilio.js

--- profile/temas/iPortfolio/assets/js/portafolio.js
+++ profile/temas/iPortfolio/assets/js/portafolio.js
@@ -6,7 +6,7 @@ async function portafolio() {
   const url = 'https://portafolio1.webcindario.com/api/v1/?tabla=portafolio';
   const res = await fetch(url);
   const data = await res.json();
-  if(!data){porta.innerHTML = ``; return;}
+  if(!res && !data){porta.innerHTML = ``; return;}
   data.reverse();//console.log(data);
   const unicos = [];
   data.forEach(item => {
