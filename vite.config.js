bbbc4ae0cc5e929b6cb0aaac1e246dcbe159cefe
multiportal <multiportal@outlook.com>
2026-09-04 14:08:39 -0600

    Update

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  /*assetsInclude: ['** /*.html'],*/
+  base: '/', //Configuar para "spa -> /" para "hash -> ./"
+  root: './',
+  build: {
+    outDir: 'profile',
+  },
+  publicDir: 'public',
+  envDir: ".env"
+})
